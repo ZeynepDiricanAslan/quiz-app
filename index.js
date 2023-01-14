2a10de38154ab60a914847183fd3e6a624b3b150
ZeynepDiricanAslan <zeynepdiricanaslan@gmail.com>
2023-01-14 21:29:16 +0300

    url search params eklendi

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+// çağırıldığında username "id"li html elemanını dom ile yakalayıp değerini(value) kontrol eden; değer var ise başka bir sayfaya yönlendiren, yoksa ekrana değer girilmesi için uyarı çıkartan bir fonksiyon yazınız. 
+
+
 function start() {
     console.log("zeynep");
     const myElement = document.getElementById("username");
     console.log(myElement.value);
-    if (myElement.value.trim() !== ""  ) {
+    if (myElement.value.trim() !== "") {
         console.log("değer var")
+        // search parametresiyle başka sayfaya yönlendirme
+        location.href = `/quiz.html?username=${myElement.value.trim()}`;
+
         // başka bir sayfaya yönlendirme
-        location.href="/quiz.html"; 
+        // location.href="/quiz.html"; 
 
     } else {
         // console.log("değer yok");
         alert("Please enter a username")
     }
+
 }
